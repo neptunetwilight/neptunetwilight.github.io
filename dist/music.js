eb7b5f69cfdd51b4805546bd9bd60920b400d788
your name <your email>
2021-02-08 17:58:20 +0800

    Site updated: 2021-02-08 17:58:13

---
+++ dist/music.js
@@ -0,0 +1,37 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+      {
+        name: '白露、荻の上風',
+        artist: '浅野隼人',
+        url: 'https://link.gimhoy.com/googledrive/aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL2ZpbGUvZC8xeEVZOE1HRWxmT210VmNvTnI0TXIzWjllcHByNGlFcDYvdmlldz91c3A9c2hhcmluZw==.mp3',
+        cover: 'http://p2.music.126.net/u0B4lPJtICaCaByIATq11g==/109951165533703560.jpg?param=300x300',
+      },
+	  {
+        name: "It's all uphill from here",
+        artist: '浅野隼人',
+        url: 'https://link.gimhoy.com/googledrive/aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL2ZpbGUvZC8xMzRlMDlPMzE1M09sQTZmdWQ0cWNQWU55VEhvRDdMbUYvdmlldz91c3A9c2hhcmluZw==.mp3',
+        cover: 'http://p2.music.126.net/u0B4lPJtICaCaByIATq11g==/109951165533703560.jpg?param=300x300',
+      },
+      {
+        name: '芒種、鼠黐の薫風',
+        artist: '浅野隼人',
+        url: 'https://link.gimhoy.com/googledrive/aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL2ZpbGUvZC8xemVHSE5YOENCUkxxVHFpcVRvMm9aWTg3ZzdKWHVrZzgvdmlldz91c3A9c2hhcmluZw==.mp3',
+        cover: 'http://p2.music.126.net/u0B4lPJtICaCaByIATq11g==/109951165533703560.jpg?param=300x300',
+      },
+	  {
+        name: 'wish',
+        artist: '浅野隼人',
+        url: 'https://link.gimhoy.com/googledrive/aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL2ZpbGUvZC8xRHZOTTJ1YXJzeTNadENGQ3B4b3pzZUpvLWVBT0tRNF8vdmlldz91c3A9c2hhcmluZw==.mp3',
+        cover: 'http://p2.music.126.net/u0B4lPJtICaCaByIATq11g==/109951165533703560.jpg?param=300x300',
+      },
+	  {
+        name: 'Order of the New World',
+        artist: '44magnum ',
+        url: 'https://link.gimhoy.com/googledrive/aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL2ZpbGUvZC8xMnloNndiNDYzMW1XRktoUTRaZVJIVm5ZMFdhVW1EWi0vdmlldz91c3A9c2hhcmluZw==.mp3',
+        cover: 'https://riyugo.com/i/2021/02/03/12nix3s.png',
+      }
+    ]
+});
