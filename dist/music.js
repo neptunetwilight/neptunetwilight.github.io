fdd52cfb58024e4cb925d2ab8de46ef6654a2247
neptunetwilight <127556048@qq.com>
2021-04-16 00:13:50 +0800

    Site updated: 2021-04-16 00:13:49

---
+++ dist/music.js
@@ -0,0 +1,37 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+      {
+        name: '白露、荻の上風',
+        artist: '浅野隼人',
+        url: 'https://files.catbox.moe/rk9cad.mp3',
+        cover: 'http://p2.music.126.net/u0B4lPJtICaCaByIATq11g==/109951165533703560.jpg?param=300x300',
+      },
+	  {
+        name: "It's all uphill from here",
+        artist: '浅野隼人',
+        url: 'https://files.catbox.moe/tpuya1.mp3',
+        cover: 'http://p2.music.126.net/u0B4lPJtICaCaByIATq11g==/109951165533703560.jpg?param=300x300',
+      },
+      {
+        name: '芒種、鼠黐の薫風',
+        artist: '浅野隼人',
+        url: 'https://files.catbox.moe/4bqkz5.mp3',
+        cover: 'http://p2.music.126.net/u0B4lPJtICaCaByIATq11g==/109951165533703560.jpg?param=300x300',
+      },
+	  {
+        name: 'wish',
+        artist: '浅野隼人',
+        url: 'https://files.catbox.moe/3smtro.mp3',
+        cover: 'http://p2.music.126.net/u0B4lPJtICaCaByIATq11g==/109951165533703560.jpg?param=300x300',
+      },
+	  {
+        name: 'Order of the New World',
+        artist: '44magnum ',
+        url: 'https://files.catbox.moe/uu476n.mp3',
+        cover: 'https://riyugo.com/i/2021/02/03/12nix3s.png',
+      }
+    ]
+});
